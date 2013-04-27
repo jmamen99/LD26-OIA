52763682d90c195d91865ae1b32a7368e7d82445
Jerre van der Hulst <git@jerreav.nl>
2013-04-27 15:06:52 +0200

    levelloader done

--- src/nl/jerreav/OIA/Grid.java
+++ src/nl/jerreav/OIA/Grid.java
@@ -24,23 +24,20 @@ public class Grid {
 		
 		tiles = new Array<Tile>();
 
-		addNewTile(new Tile(TileColor.BLUE, 4, TileShape.CORNER, 2*U.SPRITESIZE, 0*U.SPRITESIZE, 0));
-		addNewTile(new Tile(TileColor.RED, 2, TileShape.CORNER,2*U.SPRITESIZE, 4*U.SPRITESIZE, 1));
-		addNewTile(new Tile(TileColor.YELLOW, 2, TileShape.CORNER, 2*U.SPRITESIZE, 6*U.SPRITESIZE, 2));
-		addNewTile(new Tile(TileColor.BLUE, 2, TileShape.CORNER, 2*U.SPRITESIZE, 8*U.SPRITESIZE, 3));
-		addNewTile(new Tile(TileColor.YELLOW, 1, TileShape.SQUARE, 2*U.SPRITESIZE, 5*U.SPRITESIZE, 0));
-		addNewTile(new Tile(TileColor.YELLOW, 4, TileShape.LONG, 4*U.SPRITESIZE, 5*U.SPRITESIZE, 1));
+		LevelLoader.loadLevel("pics/levels/1/", this);
+//		addNewTile(new Tile(TileColor.YELLOW, 4, TileShape.SQUARE, 11*U.SPRITESIZE, 9*U.SPRITESIZE, 0));
 	}
 
-	private void addNewTile(Tile tile) {
+	public void addNewTile(Tile tile) {
 		tiles.add(tile);
 		Pixmap p = tile.pixMap;
 		for(int i = 0; i < p.getWidth(); i++){
 			for(int j = 0; j < p.getHeight(); j++){
 //				Gdx.app.log("Grid", tile.size + "no ");
 				if(p.getPixel(i, j) == Color.rgba8888(1,1,1,1)){
+//					Gdx.app.log("Grid", tile.size + " " + (tile.x/U.SPRITESIZE+i) + " " + (tile.y/U.SPRITESIZE+j+p.getHeight()-1) + " ");
 					sockets[tile.x/U.SPRITESIZE+i][tile.y/U.SPRITESIZE-j+p.getHeight()-1].tile = tile;
-					Gdx.app.log("Grid", tile.size + " " + tile.x/U.SPRITESIZE+i + " " + tile.y/U.SPRITESIZE+j + " ");
+					Gdx.app.log("Grid", tile.size + " " + (tile.x/U.SPRITESIZE+i) + " " + (tile.y/U.SPRITESIZE+j+p.getHeight()-1) + " ");
 				}
 			}
 		}
---
+++ src/nl/jerreav/OIA/LevelLoader.java
@@ -0,0 +1,58 @@
+package nl.jerreav.OIA;
+
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Color;
+import com.badlogic.gdx.graphics.Pixmap;
+
+public class LevelLoader {
+
+	private final static int red = Color.rgba8888(1,0,0,1);
+	private final static int yellow = Color.rgba8888(1,1,0,1);
+	private final static int blue = Color.rgba8888(0,0,1,1);
+	
+	
+	public static void loadLevel(String path, Grid grid){
+		
+		readPixmap(grid,path + "r_1_1.png",TileShape.SQUARE,1,0);
+		readPixmap(grid,path + "r_2_1.png",TileShape.SQUARE,2,0);
+		readPixmap(grid,path + "r_4_1.png",TileShape.SQUARE,4,0);
+//
+		readPixmap(grid,path + "c_2_1.png",TileShape.CORNER,2,0);
+		readPixmap(grid,path + "c_2_2.png",TileShape.CORNER,2,1);
+		readPixmap(grid,path + "c_2_3.png",TileShape.CORNER,2,2);
+		readPixmap(grid,path + "c_2_4.png",TileShape.CORNER,2,3);
+		
+		readPixmap(grid,path + "c_4_1.png",TileShape.CORNER,4,0);
+		readPixmap(grid,path + "c_4_2.png",TileShape.CORNER,4,1);
+		readPixmap(grid,path + "c_4_3.png",TileShape.CORNER,4,2);
+		readPixmap(grid,path + "c_4_4.png",TileShape.CORNER,4,3);
+
+		readPixmap(grid,path + "l_2_1.png",TileShape.LONG,2,0);
+		readPixmap(grid,path + "l_2_2.png",TileShape.LONG,2,1);
+		readPixmap(grid,path + "l_4_1.png",TileShape.LONG,4,0);
+		readPixmap(grid,path + "l_4_2.png",TileShape.LONG,4,1);
+		
+		
+	}
+
+	private static void readPixmap(Grid grid, String path, TileShape shape, int size, int rotation) {
+		Pixmap p = new Pixmap(Gdx.files.internal(path));
+		for(int i = 0; i < p.getWidth(); i++){
+			for(int j = 0; j < p.getHeight(); j++){
+				int c = p.getPixel(i, j);
+				if(c == red){
+					Gdx.app.log("LevelLoader", "red " + (i*U.SPRITESIZE) + "," + (U.h-j*U.SPRITESIZE-U.SPRITESIZE));
+					grid.addNewTile(new Tile(TileColor.RED, size, shape,i*U.SPRITESIZE, U.h-j*U.SPRITESIZE-U.SPRITESIZE, rotation));
+				}
+				else if(c == yellow){
+					Gdx.app.log("LevelLoader", "yellow  " + (i*U.SPRITESIZE) + "," + (U.h-j*U.SPRITESIZE-U.SPRITESIZE));
+					grid.addNewTile(new Tile(TileColor.YELLOW, size, shape,i*U.SPRITESIZE, U.h-j*U.SPRITESIZE-U.SPRITESIZE, rotation));
+				}
+				else if(c == blue){
+					Gdx.app.log("LevelLoader", "blue " + (i*U.SPRITESIZE) + "," + (U.h-j*U.SPRITESIZE-U.SPRITESIZE));
+					grid.addNewTile(new Tile(TileColor.BLUE, size, shape,i*U.SPRITESIZE, U.h-j*U.SPRITESIZE-U.SPRITESIZE, rotation));
+				}
+			}
+		}
+	}
+}
